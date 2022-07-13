93215d11fa73de544f49959440cfaa87a2a3589c
Vincent Taing <vtaing@tockhq.com>
2022-07-13 11:07:52 -0400

    Set different write keys for prod/dev env

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import Analytics from "analytics";
+import segmentPlugin from "@analytics/segment";
+
+const analytics = Analytics({
+  app: "awesome-app",
+  plugins: [
+    segmentPlugin({
+      writeKey: "123-xyz",
+    }),
+  ],
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
